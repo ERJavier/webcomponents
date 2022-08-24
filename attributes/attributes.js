c9c1b5147400d1cba0e2aa149a0437ce90e7316e
Javier Cepeda <erjavierc@gmail.com>
2022-08-23 21:24:59 -0400

    attributes

--- attributes/attributes.js
+++ attributes/attributes.js
@@ -2,18 +2,20 @@ class myElement extends HTMLElement {
     constructor(){
         super();
         this.attachShadow({mode: "open"});
+
+        this.title = this.getAttribute("title");
+        this.paragraph = this.getAttribute("paragraph");
+        this.img = this.getAttribute("img");
     }
     getTemplate(){
         const template = document.createElement('template');
         template.innerHTML = `
             <section>
-                <h2>
-                    <slot name ="title"></slot>
-                </h2>
+                <h2>${this.title}</h2>
                 <div>
-                    <p>
-                    <slot name="paragraph"></slot>
-                    </p>
+                    <p>${this.paragraph}</p>
+                    <img src=${this.img}/>
+
                 </div>
             </section>
             ${this.getStyles()}
